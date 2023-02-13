3772fd3417119e3f50450a28742556550545a239
mynameiskuun <mynameiskuun@gmail.com>
2023-02-13 21:06:10 +0900

    업데이트

--- AjaxCommentEX/src/main/java/com/example/demo/CommentDAO.java
+++ AjaxCommentEX/src/main/java/com/example/demo/CommentDAO.java
@@ -1,9 +1,13 @@
 package com.example.demo;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface CommentDAO {
 
 	public int insertComment(CommentDTO commentDTO);
+	public List<CommentDTO> selectAllComment();
+	public int deleteComment(int com_id);
 }
--- AjaxCommentEX/src/main/java/com/example/demo/HomeController.java
+++ AjaxCommentEX/src/main/java/com/example/demo/HomeController.java
@@ -1,8 +1,10 @@
 package com.example.demo;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import lombok.RequiredArgsConstructor;
 
@@ -31,7 +33,8 @@ public void feed() {
 }
 
 @GetMapping("/view")
-public void view() {
+public void view(Model model) {
+	
 }
 
 @GetMapping("/newPost")
@@ -44,7 +47,21 @@ public String insertComment(CommentDTO commDTO) {
 	
 	System.out.println("commDTO : " + commDTO.toString());
 	commentdao.insertComment(commDTO);
-	return "redirect:/view";
+	return "redirect:/selectAllComment";
 }
   
+@GetMapping("/selectAllComment")
+public String selectAllComment(Model model) {
+	
+	System.out.println("selectCommentTest");
+	List<CommentDTO> cList = commentdao.selectAllComment();
+	model.addAttribute("list", cList);
+	return "/view";
+}
+
+@GetMapping("/delete")
+public String deleteComment(int com_id) {
+	commentdao.deleteComment(com_id);
+	return "redirect:/selectAllComment";
+}
 } 
